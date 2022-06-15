09c296fdecb5c0d96128aef1a882c809d5f13a93
thoma138 <thomaselliott@live.com>
2022-06-15 13:08:22 +1000

    commit tree fc590daa8c1cd797ba9416eb03b7194b22b69e32

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+
+const driver = {
+    name: "Sam",
+    address: "11 Broadway"
+};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const newDriver = {...driver};
+    key = address;
+    value = '11 Broadway';
+    newDriver[address] = value;
+
+    return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    return driver.key = value;
+}
